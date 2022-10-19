15ca2cc5296b2c5db690a43fa5d91b07455073fc
Remix Workspace <>
2022-10-19 13:23:43 +0530

    integrate withdrawal function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,44 +1,61 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 50 * 1e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
     function fund() public payable{
 
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough!!");
+        // require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough!!");
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!!");
         //18 decimals
         funders.push(msg.sender); //To store all sender list
         addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    function getPrice() public view returns(uint256){
+    function withdraw() public {
 
-        //ABI
-        //Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e -- ETH/USD 
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        // (/*uint80 roundID*/, int price, /*uint startedAt*/, /*uint timeStamp*/, /*uint80 answeredInRound*/) = priceFeed.latestRoundData();
-        (, int256 price,,,) = priceFeed.latestRoundData();
-        //ETH in terms of USD
-        //3000.00000000
+        // require(msg.sender == owner, "Sender is not owner")
 
-        return uint256(price * 1e10); // 1**10 = 10000000000
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        //reset the array
+        funders = new address[](0);
+
+        //msg.sender = address
+        //payable(msg.sender) = payable address
+
+        //transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        //send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        //require(sendSuccess, "Send Failed")
+
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
     }
 
-    function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        return priceFeed.version();
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
+    modifier onlyOwner {
+        require(msg.sender == owner, "Sender is not owner");
+        _; // doing the rest of the code
     }
 
     // function withdraw(){}
---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter{
+
+    function getPrice() internal view returns(uint256){
+
+        //ABI
+        //Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e -- ETH/USD 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        // (/*uint80 roundID*/, int price, /*uint startedAt*/, /*uint timeStamp*/, /*uint80 answeredInRound*/) = priceFeed.latestRoundData();
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        //ETH in terms of USD
+        //3000.00000000
+
+        return uint256(price * 1e10); // 1**10 = 10000000000
+    }
+
+    function getVersion() internal view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
